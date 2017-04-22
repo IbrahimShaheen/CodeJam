3b3605cc1a4b5a604dcd9a5f890bd3d25b23b934
IbrahimShaheen <ishaheen1995@gmail.com>
2017-04-22 14:34:17 -0400

    1B 2016

---
+++ 2016/Round 1B/A/A-Large.cs
@@ -0,0 +1,77 @@
+﻿using static System.Int32;
+using static System.IO.File;
+using static System.String;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    const string InputFilePath = "A-large.in";
+    const string OutputFilePath = "A-LargeOutput.txt";
+
+    public static double MinTime { get; private set; }
+
+    static void Main(string[] args)
+    {
+        string[] InputFile = ReadAllLines(InputFilePath);
+        int TestCases = Parse(InputFile[0]);
+
+        string[] OutputFile = new string[TestCases];
+        int ReadLocation = 1;
+        for (int i = 0; i < TestCases; i++)
+        {
+            int LinesToRead = Parse(InputFile[ReadLocation].Split(' ')[1]);
+            string[][] Case = GetCase(InputFile, ReadLocation, LinesToRead + 1);
+            string Solution = SolveCase(Case);
+            PutCaseSolution(OutputFile, i + 1, i, Solution);
+            ReadLocation += LinesToRead + 1;
+        }
+        WriteAllText(OutputFilePath, Join("\r\n", OutputFile));
+    }
+
+    // Writes each row of Solution as a line, joined by spaces, to OutputFile starting at WriteLocation
+    // Inserts a line at WriteLocation - 1 with a case label indicated by CaseNum
+    static void PutCaseSolution(string[] OutputFile, int CaseNum, int WriteLocation, string Solution)
+    {
+        OutputFile[WriteLocation] = "Case #" + CaseNum + ": " + Solution;
+        
+    }
+
+    // Returns a 2D string array which:
+    //      Contains consecutive lines of InputFile starting from ReadLocation
+    //      Has Length number of rows, each being a line in InputFile
+    //      Each row in the array contains a line delimited by spaces
+    static string[][] GetCase(string[] InputFile, int ReadLocation, int Length)
+    {
+        string[][] Result = new string[Length][];
+        for (int i = 0; i < Length; i++)
+        {
+            Result[i] = InputFile[ReadLocation + i].Split(' ');
+        }
+        return Result;
+    }
+
+    // Returns a 2D string array which is the solution to the given 2D string input
+    static string SolveCase(string[][] Input)
+    {
+        int Destination = Parse(Input[0][0]);
+        int NumOfHourses = Parse(Input[0][1]);
+        SortedDictionary<int, int> LocationSpeedPairs = new SortedDictionary<int, int>();
+        for (int i = 0; i < NumOfHourses; i++)
+        {
+            int Location = Parse(Input[i + 1][0]);
+            int Speed = Parse(Input[i + 1][1]);
+            LocationSpeedPairs.Add(Location, Speed);
+        }
+        double MaxTime = double.MinValue;
+        for (int i = NumOfHourses - 1; i >= 0; i--)
+        {
+            int Location = LocationSpeedPairs.ElementAt(i).Key;
+            int Speed = LocationSpeedPairs.ElementAt(i).Value;
+            double Time = (double)(Destination - Location) / Speed;
+            if (Time > MaxTime) MaxTime = Time;
+        }
+        return "" + Destination / MaxTime;
+    }
+}
---
+++ 2016/Round 1B/B/B-SmallAttempt1.cs
@@ -0,0 +1,81 @@
+﻿using static System.Int32;
+using static System.IO.File;
+using static System.String;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Program
+{
+    const string InputFilePath = "B-small-attempt1.in";
+    const string OutputFilePath = "B-small-attempt1.txt";
+
+    public static object LessColor { get; private set; }
+
+    static void Main(string[] args)
+    {
+        string[] InputFile = ReadAllLines(InputFilePath);
+        int TestCases = Parse(InputFile[0]);
+
+        string[] OutputFile = new string[TestCases];
+        for (int i = 0; i < TestCases; i++)
+        {
+            string CaseString = "Case #" + (i + 1) + ": ";
+            string Solution = SolveCase(InputFile[i + 1]);
+            OutputFile[i] = CaseString + Solution;
+        }
+        WriteAllText(OutputFilePath, Join("\r\n", OutputFile));
+    }
+
+    // Takes in an input line and returns the appropriate output line
+    static string SolveCase(string Input)
+    {
+        int N = Parse(Input.Split(' ')[0]);
+        string[] Result = new string[N];
+        Dictionary<string, int> CountColorPair = new Dictionary<string, int>();
+        CountColorPair.Add("R", Parse(Input.Split(' ')[1]));
+        CountColorPair.Add("Y", Parse(Input.Split(' ')[3]));
+        CountColorPair.Add("B", Parse(Input.Split(' ')[5]));
+        if (!Possible(CountColorPair)) return "IMPOSSIBLE";
+        for (int i = 0; i < N / 2; i++)
+        {
+            var List = GetSorted(CountColorPair);
+            var MaxColor = List.ElementAt(2);
+            var LessMaxColor = List.ElementAt(1);
+            Result[2 * i] = MaxColor.Key;
+            Result[2 * i + 1] = LessMaxColor.Key;
+            CountColorPair[MaxColor.Key]--;
+            CountColorPair[LessMaxColor.Key]--;
+        }
+        if (N % 2 != 0)
+        {
+            var List = GetSorted(CountColorPair);
+            var MaxColor = List.ElementAt(2);
+            Result[N - 1] = MaxColor.Key;
+        }
+        if (Result[0] == Result[N - 1])
+        {
+            string temp = Result[N - 1];
+            Result[N - 1] = Result[N - 2];
+            Result[N - 2] = temp;
+        }
+        return Join("", Result);
+    }
+
+    private static bool Possible(Dictionary<string, int> CountColorPair)
+    {
+        var List = GetSorted(CountColorPair);
+        if (List.ElementAt(2).Value > List.ElementAt(1).Value + List.ElementAt(0).Value)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private static List<KeyValuePair<string, int>> GetSorted(Dictionary<string, int> aDictionary)
+    {
+        List<KeyValuePair<string, int>> myList = aDictionary.ToList();
+        myList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+        return myList;
+    }
+}
